c103f591bb41c01a5845eabdbd437e918f6b9637
PereiraGus <gustavo.oito@hotmail.com>
2023-10-22 15:31:46 -0300

    style(): Adequação da dashboard geral ao protótipo

---
+++ public/dashboard/js/dashboardGeral.js
@@ -0,0 +1,9 @@
+function carregarDashboardGeral(){
+    now = new Date()
+    dataAtual = 
+    now.getFullYear()+
+    "-"+(now.getMonth()+1)+
+    "-"+now.getDate()
+
+    pesquisaData.value=dataAtual
+}
