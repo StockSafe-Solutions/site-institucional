8818a3a1372df8b8fea039a5d04f932063e31b2f
Rafaqwert <rafael.rrodrigues@sptech.school>
2023-11-11 17:44:11 -0300

    fix(src/model): Correção de função

--- src/models/funcionarioModel.js
+++ src/models/funcionarioModel.js
@@ -62,8 +62,8 @@ function enviarEmail(email, funcao) {
     service: "outlook",
     auth: {
       user: "stephany.justino@sptech.school",
-      pass: "#Gf24007915890",
-    },
+      pass: "#Gf24007915890"
+    }
   });
 
   var mailOptions = {
@@ -74,31 +74,32 @@ function enviarEmail(email, funcao) {
     Seja bem - vindo.
     http://localhost:3333/dashboard/testeFormColaborador.html
      `,
-    html: `<!DOCTYPE html>
-          <html lang="pt-br">
-          <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>Seja bem - vindo</title>
-          </head>
-          <body>
-            <h1>Para acessar</h1>
-            <p>É um prazer ter você conosco! Seja bem-vindo à nossa comunidade.</p>
-            <p>Para aproveitar ao máximo sua experiência, pedimos que complete seu cadastro clicando no link abaixo:</p>
-            <a href="http://localhost:3333/dashboard/testeFormColaborador.html">Complete Seu Cadastro</a>
-        
-            <p>Estamos ansiosos para tê-lo totalmente integrado à nossa plataforma. Se precisar de assistência ou tiver alguma dúvida, não hesite em entrar em contato conosco.</p>
-        
-            <p>Obrigado por fazer parte da nossa comunidade!</p>
-            <p>Email de login: ${email}</p>
-            <p>Senha: urubu100</p>
-        
-            <p>Atenciosamente,</p>
-            <p>Stocksafe<p>
-          </body>
-      
-        </html>
-      `,
+    html:`
+    <!DOCTYPE html>
+      <html lang="pt-br">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Seja bem - vindo</title>
+      </head>
+      <body>
+        <h1>Para acessar</h1>
+        <p>É um prazer ter você conosco! Seja bem-vindo à nossa comunidade.</p>
+        <p>Para aproveitar ao máximo sua experiência, pedimos que complete seu cadastro clicando no link abaixo:</p>
+        <a href="http://localhost:3333/dashboard/testeFormColaborador.html">Complete Seu Cadastro</a>
+    
+        <p>Estamos ansiosos para tê-lo totalmente integrado à nossa plataforma. Se precisar de assistência ou tiver alguma dúvida,
+          não hesite em entrar em contato conosco.</p>
+    
+        <p>Obrigado por fazer parte da nossa comunidade!</p>
+        <p>Email de login: ${email}</p>
+        <p>Senha: urubu100</p>
+    
+        <p>Atenciosamente,</p>
+        <p>Stocksafe<p>
+      </body>
+    </html>
+    `
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
