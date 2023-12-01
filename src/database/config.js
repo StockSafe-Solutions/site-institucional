28e68d97532e0a7d232ef000a65e5f2d0fd9618f
PereiraGus <gustavo.oito@hotmail.com>
2023-12-01 14:24:40 +0000

    feat(): Conexão com AWS

--- src/database/config.js
+++ src/database/config.js
@@ -3,17 +3,15 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
+    port: parseInt(1433,10),
+    server: "44.221.134.15",
+    database: "StockSafe",
+    user: "sa",
+    password: "urubu100",
+    stream: false,
     options: {
-        encrypt: true, // for azure
+        encrypt: false, // for azure
+        trustServerCertificate: true
     }
 }
 
