599ef29705ed395dd25ab8618879fd84fb0b6a7e
Rafaqwert <rafael.rrodrigues@sptech.school>
2023-12-02 19:26:01 -0300

    fix(dashboard): Grafico atualiza

--- public/dashboard/dashsEspecificas/processos.js
+++ public/dashboard/dashsEspecificas/processos.js
@@ -1,3 +1,6 @@
+var graficoExiste = false;
+var graficoProcesso;
+
 function carregarPaginaProcessos(){
     indiceParm = location.href.indexOf('?');
     params = location.href.slice(indiceParm+1,indiceParm+7);
@@ -10,6 +13,7 @@ function carregarPaginaProcessos(){
 
 function carregarProcessos(processos) {
     var tbody = document.getElementById("cell_processos");
+    document.getElementById('qtdProcessos').textContent = processos.length;
     tbody.innerHTML = ""
 
     processos.forEach((processo, i) => {
@@ -42,41 +46,60 @@ function atualizarKpis(valor) {
     const baseKpi3 = document.getElementById("baseKPI3");
     const baseKpi4 = document.getElementById("baseKPI4");
 
-    kpi1.innerHTML = parseInt(valor[0].uso_total_cpu) + "%";
-    kpi2.innerHTML = parseInt(valor[0].uso_total_ram) + "%";
-    kpi3.innerHTML = valor[0].nome_cpu;
-    kpi4.innerHTML = valor[0].nome_ram;
+    if (valor[0] == undefined){
 
-    baseKpi3.innerHTML = `com ${parseInt(valor[0].proc_total_cpu)}% de uso`
-    baseKpi4.innerHTML = `com ${parseInt(valor[0].proc_total_ram)}% de uso` 
+        kpi1.innerHTML = "0%";
+        kpi2.innerHTML = "0%";
+        kpi3.innerHTML = "Nenhum";
+        kpi4.innerHTML = "Nenhum";
+
+        baseKpi3.innerHTML = `0% de uso`
+        baseKpi4.innerHTML = `0% de uso`
+    } else {
+
+        kpi1.innerHTML = parseInt(valor[0].uso_total_cpu) + "%";
+        kpi2.innerHTML = parseInt(valor[0].uso_total_ram) + "%";
+        kpi3.innerHTML = valor[0].nome_cpu;
+        kpi4.innerHTML = valor[0].nome_ram;
+
+        baseKpi3.innerHTML = `com ${parseInt(valor[0].proc_total_cpu)}% de uso`
+        baseKpi4.innerHTML = `com ${parseInt(valor[0].proc_total_ram)}% de uso` 
+    }
 }
 
 function carregarGraficoProcesso(valor) {
-
-    var nome = [];
-    var qtd = [];
+    var nomeDosProcessos = [];
+    var quantidadeListada = [];
 
     for (i in valor) {
-        nome.push(valor[i].nome);
-        qtd.push(valor[i].quantidade);
+        nomeDosProcessos.push(valor[i].nome);
+        quantidadeListada.push(valor[i].quantidade);
     }
-    
+
     var graficoProc = document.getElementById('graficoProcessos');
-	return new Chart(graficoProc, {
-		type: "bar",
-		data: {
-            labels: nome,
-			datasets: [
-				{
-					label: "Processos",
-					data: qtd,
+
+    if (graficoProcesso) {
+        graficoProcesso.destroy();
+    }
+
+    graficoProcesso = new Chart(graficoProc, {
+        type: "bar",
+        data: {
+            labels: nomeDosProcessos,
+            datasets: [
+                {
+                    label: "Processos",
+                    data: quantidadeListada,
                     backgroundColor: ["#00093f", "#000d63", "#011387", "#011abc", "#0020f4"],
-					hoverOffset: 4
+                    hoverOffset: 4
                 }
             ]
         }
     });
+
+    return graficoProcesso;
 }
+
 
 function atualizarDadosProcessos(params) {
 
@@ -103,27 +126,6 @@ function atualizarDadosProcessos(params) {
         console.log(erro);
     })
 
-    fetch(`/processo/atualizarGraficoProc/${codServidor}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(function (resposta) {
-        if (resposta.ok) {
-            console.log(resposta);
-            resposta.json().then(json => {
-                carregarGraficoProcesso(json)
-            });
-        }
-        else {
-            resposta.text().then(texto => {
-                console.warn(texto)
-            })
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-    })
-
     fetch(`/processo/listarProcessos/${codServidor}`, {
         method: "GET",
         headers: {
@@ -135,6 +137,27 @@ function atualizarDadosProcessos(params) {
             resposta.json().then(json => {
                 console.log(json)
                 carregarProcessos(json)
+            });
+        }
+        else {
+            resposta.text().then(texto => {
+                console.warn(texto)
+            })
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    fetch(`/processo/atualizarGraficoProc/${params}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(function (resposta) {
+        if (resposta.ok) {
+            console.log(resposta);
+            resposta.json().then(json => {
+                carregarGraficoProcesso(json)
             });
         }
         else {
--- src/models/processoModel.js
+++ src/models/processoModel.js
@@ -1,37 +1,47 @@
 var database = require("../database/config")
 
 function listarProcessos(codServidor){
-    var instrucao = `SELECT proc.*,
+    var instrucao = 
+    `
+    SELECT proc.*,
     serv.codigo
-    FROM tb_processo as proc
-    JOIN tb_servidor as serv
+    FROM tb_processo AS proc
+    JOIN tb_servidor AS serv 
     ON proc.fk_servidor = serv.id_servidor
-    WHERE serv.codigo = '${codServidor}'
-    ORDER BY nome_proc;`
-
+    WHERE serv.codigo = '${codServidor}' 
+    AND data_hora >= NOW() - INTERVAL 1 MINUTE
+    ORDER BY proc.nome_proc;
+    `
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
 function atualizarKpis(codServidor){
-    var instrucao = `SELECT * 
+    var instrucao = 
+    `
+    SELECT * 
     FROM vw_proc_kpi
-    WHERE codigo = '${codServidor}';`
+    WHERE codigo = '${codServidor}';
+    `
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
 function atualizarGraficoProc(codServidor){
-    var instrucao = `SELECT proc.nome_proc AS nome, 
+    var instrucao = 
+    `
+    SELECT proc.nome_proc AS nome, 
     COUNT(proc.nome_proc) AS quantidade,
     serv.codigo AS codigo
     FROM tb_processo AS proc
     JOIN tb_servidor AS serv
     ON proc.fk_servidor = serv.id_servidor
     WHERE codigo = '${codServidor}'
+    AND DATE(proc.data_hora) = CURDATE()
     GROUP BY nome_proc,
     codigo
-    ORDER BY quantidade DESC LIMIT 5;`
+    ORDER BY quantidade DESC LIMIT 5;
+    `
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
